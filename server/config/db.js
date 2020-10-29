bd9e0a540b218700ede293c0c50cbff5a204bb25
Dom <dnovell1@my.centennialcollege.ca>
2020-10-29 14:30:05 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+
+  "URI": "mongodb+srv://mingo101:2fgHq9Y8YWvHaxgd@favourite.8xjif.mongodb.net/favourite?retryWrites=true&w=majority"
+  //2fgHq9Y8YWvHaxgd
+};
